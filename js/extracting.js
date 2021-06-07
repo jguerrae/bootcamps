e6e839b95731a59814d0f874307cac73ccda0469
Juan Vargas <juan_vargas10@hotmail.com>
2021-06-06 23:01:23 -0500

    save file feature

--- js/extracting.js
+++ js/extracting.js
@@ -151,6 +151,47 @@ function getJobHistory() {
   return TotalLine;
 }
 function makeLine() {
-  console.log(getEducation() + "," + getJobTitle() + "," + getJobHistory());
+  const info = getEducation() + "," + getJobTitle() + "," + getJobHistory();
+  console.log(info);
+  return info;
 }
-makeLine();
+const saveFile = function (data, filename) {
+  if (!data) {
+    console.error("Console.save: No data");
+    return;
+  }
+
+  if (!filename) filename = "150 de .txt";
+
+  if (typeof data === "object") {
+    data = JSON.stringify(data, undefined, 4);
+  }
+
+  var blob = new Blob([data], { type: "text/json" }),
+    e = document.createEvent("MouseEvents"),
+    a = document.createElement("a");
+
+  a.download = filename;
+  a.href = window.URL.createObjectURL(blob);
+  a.dataset.downloadurl = ["text/json", a.download, a.href].join(":");
+  e.initMouseEvent(
+    "click",
+    true,
+    false,
+    window,
+    0,
+    0,
+    0,
+    0,
+    0,
+    false,
+    false,
+    false,
+    false,
+    0,
+    null
+  );
+  a.dispatchEvent(e);
+};
+const info = makeLine();
+saveFile(info);
